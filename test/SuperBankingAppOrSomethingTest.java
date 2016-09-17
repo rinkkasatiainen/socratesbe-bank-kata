18e8fdec1d0e0939579b37028e65fa531a7e96fe
Aki Salmi <aki.salmi@iki.fi>
2016-09-17 18:11:57 +0300

    can deposit

--- test/SuperBankingAppOrSomethingTest.java
+++ test/SuperBankingAppOrSomethingTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.ExcludeCategories;
@@ -8,18 +9,27 @@ import static org.junit.Assert.fail;
 
 public class SuperBankingAppOrSomethingTest {
 
+    private Account account;
+
+    @Before
+    public void setUp() throws Exception {
+        account = new Account( );
+    }
 
     @org.junit.Test
     public void A_new_account_is_empty() throws Exception {
-        Account account = new Account( );
-
         assertThat(account.isEmpty(), equalTo( true ));
     }
 
     @Test
-    public void An_account_cannot_be_overdrawn() throws Exception {
-        Account account = new Account();
+    public void We_can_withdraw_from_an_account() throws Exception {
+        account.deposit( new Money(10));
 
+        account.withdraw( new Money(1) );
+    }
+
+    @Test
+    public void An_account_cannot_be_overdrawn() throws Exception {
         Money money = new Money(1);
         try {
             account.withdraw( money );
@@ -27,17 +37,24 @@ public class SuperBankingAppOrSomethingTest {
             return;
         }
         fail("Expected AccountCannotBeOverdrawn");
-
     }
 
     private class Account {
+        private boolean hasDeposited;
+
         public boolean isEmpty() {
             return true;
         }
 
 
         public void withdraw(Money money) {
-            throw new AccountCannotBeOverdrawn();
+            if( !hasDeposited ){
+                throw new AccountCannotBeOverdrawn();
+            }
+        }
+
+        public void deposit(Money money) {
+            hasDeposited = true;
         }
     }
 
